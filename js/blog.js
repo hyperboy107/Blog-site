3df2a63a77c68a58d0dc8d21e8feab00cb27ee53
master <hyperboy107@gmail.com>
2024-06-22 00:07:34 +0530

    first commit

---
+++ js/blog.js
@@ -0,0 +1,54 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import { getDatabase,get,ref } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-database.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBKayhSJN9Wc-Pd_bxeXV8pV-5hSbeY2Bc",
+    authDomain: "blogg-84dda.firebaseapp.com",
+    projectId: "blogg-84dda",
+    storageBucket: "blogg-84dda.appspot.com",
+    messagingSenderId: "807401861335",
+    appId: "1:807401861335:web:f8884dc6aade1b04184092"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+const db = getDatabase(app)
+
+
+
+
+function getPostData (){
+    const user_ref = ref(db,'post/');
+    get(user_ref).then((snapshot)=>{
+        const data = snapshot.val()
+       
+       let html = "";
+       const table = document.querySelector('.main')
+        for (const key in data){
+          const{title,post_content} = data[key]
+  
+          console.log(post_content);
+         
+         
+           html+= `
+           <div class="post"> 
+               <h2>${title}</h2>
+               <p>
+                 ${post_content}
+               </p>
+           </div>
+          `
+  
+        }
+  table.innerHTML =html
+  
+    })
+  }
+  
+  getPostData()
+  
